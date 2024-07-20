99c7ee9e02415a8434c8c68e1bbff419535229a4
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-20 17:54:28 +0900

    [FEAT]: 회원가입 구현(일반, 관리자)

--- src/main/java/com/sparta/filmfly/domain/user/entity/User.java
+++ src/main/java/com/sparta/filmfly/domain/user/entity/User.java
@@ -1,12 +1,16 @@
 package com.sparta.filmfly.domain.user.entity;
 
 import com.sparta.filmfly.global.common.TimeStampEntity;
+import com.sparta.filmfly.global.common.response.ResponseCodeEnum;
+import com.sparta.filmfly.global.exception.custom.detail.AccessDeniedException;
+import com.sparta.filmfly.global.exception.custom.detail.InformationMismatchException;
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Entity
@@ -27,7 +31,7 @@ public class User extends TimeStampEntity {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String nickname;
 
     @Enumerated(EnumType.STRING)
@@ -41,7 +45,6 @@ public class User extends TimeStampEntity {
     @Column
     private String introduce;
 
-    // 자주 사용해서 아예 user entity 에 저장
     @Column
     private String pictureUrl;
 
@@ -50,4 +53,47 @@ public class User extends TimeStampEntity {
 
     @Column
     private Long kakaoId;
+
+    // 일반 회원가입 유저 생성
+    @Builder
+    public User(String username, String password, String email, UserStatusEnum userStatus, UserRoleEnum userRole) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.userStatus = userStatus;
+        this.userRole = userRole;
+    }
+
+    // 사용자가 탈퇴,정지,이메일 인증안된 상태인지 검증
+    public void validateUserStatus() {
+        if (this.userStatus == UserStatusEnum.DELETED) {
+            throw new AccessDeniedException(ResponseCodeEnum.USER_DELETED);
+        } else if (this.userStatus == UserStatusEnum.SUSPENDED) {
+            throw new AccessDeniedException(ResponseCodeEnum.USER_SUSPENDED);
+        } else if (this.userStatus == UserStatusEnum.UNVERIFIED){
+            throw new AccessDeniedException(ResponseCodeEnum.EMAIL_VERIFICATION_REQUIRED);
+        }
+    }
+
+    // 비밀번호가 일치하는지 검증
+    public void validatePassword(String rawPassword, PasswordEncoder passwordEncoder) {
+        if (!passwordEncoder.matches(rawPassword, this.password)) {
+            throw new InformationMismatchException(ResponseCodeEnum.PASSWORD_INCORRECT);
+        }
+    }
+
+    // 리프레시 토큰 업데이트
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+
+    // 인증된 상태로 변경하는 메서드
+    public void updateVerified() {
+        if (this.userStatus != UserStatusEnum.UNVERIFIED) {
+            throw new IllegalStateException("User is already verified or in an invalid state.");
+        }
+        this.userStatus = UserStatusEnum.VERIFIED;
+    }
 }
+
--- src/main/java/com/sparta/filmfly/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/filmfly/domain/user/repository/UserRepository.java
@@ -5,10 +5,31 @@ import com.sparta.filmfly.global.common.response.ResponseCodeEnum;
 import com.sparta.filmfly.global.exception.custom.detail.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // 사용자 ID로 유저를 찾고 없으면 예외 발생
     default User findByIdOrElseThrow(Long userId) {
         return findById(userId)
-            .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.USER_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.USER_NOT_FOUND));
+    }
+
+    // 사용자 이름으로 유저를 찾기
+    Optional<User> findByUsername(String username);
+
+    // 사용자 이름으로 유저를 찾고 없으면 예외 발생
+    default User findByUsernameOrElseThrow(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.USER_NOT_FOUND));
+    }
+
+    // 이메일로 유저를 찾기
+    Optional<User> findByEmail(String email);
+
+    // 이메일로 유저를 찾고 없으면 예외 발생
+    default User findByEmailOrElseThrow(String email) {
+        return findByEmail(email)
+                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.USER_NOT_FOUND));
     }
 }
