b7497eecc4cb673b37f951871fb4a51088512aab
원지연 <dnjs229@gmail.com>
2024-07-21 16:32:27 +0900

    [FEAT] 싫어요 등록 구현 완료

--- src/main/java/com/sparta/filmfly/domain/reaction/controller/GoodController.java
+++ src/main/java/com/sparta/filmfly/domain/reaction/controller/GoodController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/goods")
+@RequestMapping("/good")
 public class GoodController {
 
     private final GoodService goodService;
--- src/main/java/com/sparta/filmfly/domain/reaction/repository/BadRepository.java
+++ src/main/java/com/sparta/filmfly/domain/reaction/repository/BadRepository.java
@@ -1,8 +1,11 @@
 package com.sparta.filmfly.domain.reaction.repository;
 
+import com.sparta.filmfly.domain.reaction.ReactionContentTypeEnum;
 import com.sparta.filmfly.domain.reaction.entity.Bad;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BadRepository extends JpaRepository<Bad, Long> {
 
+    Optional<Bad> findByTypeIdAndType(Long contentId, ReactionContentTypeEnum contentType);
 }
