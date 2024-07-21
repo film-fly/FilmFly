7dd474dc65e67b1d1b7cfa4f8863157901dc2e58
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-07-21 17:49:35 +0900

    [FEAT] 보드 조회 생성

--- src/main/java/com/sparta/filmfly/domain/board/controller/BoardController.java
+++ src/main/java/com/sparta/filmfly/domain/board/controller/BoardController.java
@@ -1,18 +1,24 @@
 package com.sparta.filmfly.domain.board.controller;
 
+import com.sparta.filmfly.domain.board.dto.BoardPageResponseDto;
 import com.sparta.filmfly.domain.board.dto.BoardRequestDto;
 import com.sparta.filmfly.domain.board.dto.BoardResponseDto;
+import com.sparta.filmfly.domain.board.entity.Board;
 import com.sparta.filmfly.domain.board.service.BoardService;
 import com.sparta.filmfly.global.common.response.DataResponseDto;
 import com.sparta.filmfly.global.common.response.ResponseUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -31,6 +37,27 @@ public class BoardController {
             @Valid @RequestBody BoardRequestDto requestDto
     ) {
         BoardResponseDto responseDto = boardService.create(requestDto);
+        return ResponseUtils.success(responseDto);
+    }
+
+    // 조회
+    @GetMapping("/{boardId}")
+    public ResponseEntity<DataResponseDto<BoardResponseDto>> read(
+            @PathVariable Long boardId
+    ) {
+        BoardResponseDto responseDto = boardService.read(boardId);
+        return ResponseUtils.success(responseDto);
+    }
+
+    // 페이징 조회
+    // http://localhost:8080/board/page?pageNum=1&size=5
+    @GetMapping("/page")
+    public ResponseEntity<DataResponseDto<BoardPageResponseDto>> reads(
+            @RequestParam(value = "pageNum", required = false, defaultValue = "1") final Integer pageNum,
+            @RequestParam(value = "size", required = false, defaultValue = "5") final Integer size
+    ) {
+        Pageable pageable = PageRequest.of(pageNum-1, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        BoardPageResponseDto responseDto = boardService.reads(pageable);
         return ResponseUtils.success(responseDto);
     }
 
