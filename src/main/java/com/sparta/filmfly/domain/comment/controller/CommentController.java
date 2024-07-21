71132db3c7859bf96527f99a6d30ccdda74fe705
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-07-21 23:00:09 +0900

    [FEAT] 댓글 생성

--- src/main/java/com/sparta/filmfly/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/filmfly/domain/comment/controller/CommentController.java
@@ -1,14 +1,36 @@
 package com.sparta.filmfly.domain.comment.controller;
 
+import com.sparta.filmfly.domain.comment.dto.CommentRequestDto;
+import com.sparta.filmfly.domain.comment.dto.CommentResponseDto;
+import com.sparta.filmfly.domain.comment.service.CommentService;
+import com.sparta.filmfly.global.common.response.DataResponseDto;
+import com.sparta.filmfly.global.common.response.ResponseUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/exam")
+@RequestMapping("/board/{boardId}/comment")
 public class CommentController {
+    private final CommentService commentService;
+
+    //댓글 생성
+    @PostMapping
+    public ResponseEntity<DataResponseDto<CommentResponseDto>> createComment(
+            @PathVariable Long boardId,
+            @Valid @RequestBody CommentRequestDto requestDto
+            //@AuthenticationPrincipal UserDetailsImpl userDetails,
+    ) {
+        CommentResponseDto responseDto = commentService.createComment(boardId,requestDto);
+        return ResponseUtils.success(responseDto);
+    }
 
 }
--- src/main/java/com/sparta/filmfly/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/filmfly/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.filmfly.domain.comment.repository;
 
 import com.sparta.filmfly.domain.comment.entity.Comment;
+import com.sparta.filmfly.global.common.response.ResponseCodeEnum;
+import com.sparta.filmfly.global.exception.custom.detail.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentQueryRepository {
+    default Comment findByIdOrElseThrow(Long commentId) {
+        return findById(commentId)
+                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.COMMENT_NOT_FOUND));
+    }
 }
