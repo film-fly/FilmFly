c30ceff803f44c3dd24913d69f5024b69df65c80
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-07-21 23:23:14 +0900

    [FEAT] 댓글 조회 생성

--- src/main/java/com/sparta/filmfly/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/filmfly/domain/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.sparta.filmfly.domain.comment.service;
 
 import com.sparta.filmfly.domain.board.entity.Board;
 import com.sparta.filmfly.domain.board.repository.BoardRepository;
+import com.sparta.filmfly.domain.comment.dto.CommentPageResponseDto;
 import com.sparta.filmfly.domain.comment.dto.CommentRequestDto;
 import com.sparta.filmfly.domain.comment.dto.CommentResponseDto;
 import com.sparta.filmfly.domain.comment.entity.Comment;
@@ -10,6 +11,8 @@ import com.sparta.filmfly.domain.user.entity.User;
 import com.sparta.filmfly.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,5 +36,21 @@ public class CommentService {
         Comment savedComment = commentRepository.save(entity);
 
         return CommentResponseDto.fromEntity(savedComment);
+    }
+
+    public CommentResponseDto readComment(Long boardId, Long commentId) {
+        Board board = boardRepository.findByIdOrElseThrow(boardId); //게시판 경로 확인
+        Comment comment = commentRepository.findByIdOrElseThrow(commentId);
+
+        return CommentResponseDto.fromEntity(comment);
+    }
+
+    public CommentPageResponseDto readsComment(Long boardId, Pageable pageable) {
+        Board board = boardRepository.findByIdOrElseThrow(boardId); //게시판 경로 확인
+        Page<Comment> comments = commentRepository.findAll(pageable);
+        //QueryDSL 최적화 수정
+
+        //List 형식 totalPages,size,content,number 등 필요한 정보만 보내는 PageResponse
+        return CommentPageResponseDto.fromPage(comments);
     }
 }
