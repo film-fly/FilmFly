dac5b6e2e09432a45bb2e9072dcaa3f8b4fd152b
cthj1234 <ghwls8172@naver.com>
2024-07-22 14:14:37 +0900

    [FEAT]:
    User 검증 기능 추가
    
    - UserDetails를 통한 User 검증 추가

--- src/main/java/com/sparta/filmfly/domain/coupon/service/CouponService.java
+++ src/main/java/com/sparta/filmfly/domain/coupon/service/CouponService.java
@@ -1,9 +1,12 @@
 package com.sparta.filmfly.domain.coupon.service;
 
+import static com.sparta.filmfly.domain.user.entity.UserRoleEnum.ROLE_ADMIN;
+
 import com.sparta.filmfly.domain.coupon.dto.CouponRequestDto;
 import com.sparta.filmfly.domain.coupon.dto.CouponResponseDto;
 import com.sparta.filmfly.domain.coupon.entity.Coupon;
 import com.sparta.filmfly.domain.coupon.repository.CouponRepository;
+import com.sparta.filmfly.domain.user.entity.User;
 import com.sparta.filmfly.global.common.response.ResponseCodeEnum;
 import com.sparta.filmfly.global.exception.custom.detail.NotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +20,10 @@ public class CouponService {
     private final CouponRepository couponRepository;
 
     @Transactional
-    public CouponResponseDto createCoupon(CouponRequestDto requestDto) {
+    public CouponResponseDto createCoupon(User user,CouponRequestDto requestDto) {
+
+        validUser(user);
+
         Coupon coupon = Coupon.builder()
                 .requestDto(requestDto)
                 .build();
@@ -35,12 +41,19 @@ public class CouponService {
         return CouponResponseDto.fromEntity(coupon);
     }
 
-    public CouponResponseDto deleteCoupon(Long id) {
+    public CouponResponseDto deleteCoupon(User user, Long id) {
+        validUser(user);
         Coupon coupon = couponRepository.findById(id).orElseThrow(
                 () -> new NotFoundException(ResponseCodeEnum.COUPON_NOT_FOUND)
         );
 
         couponRepository.delete(coupon);
         return CouponResponseDto.fromEntity(coupon);
+    }
+
+    public void validUser(User user) {
+        if (user.getUserRole() != ROLE_ADMIN) {
+            throw new UnAuthorizedException(ResponseCodeEnum.USER_UNAUTHORIZED);
+        }
     }
 }
