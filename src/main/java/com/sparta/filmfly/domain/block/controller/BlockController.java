6a540fade6eba79553245da6c8f19771da600ff1
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-23 02:16:21 +0900

    [FEAT] 내가 차단한 유저 목록 조회

--- src/main/java/com/sparta/filmfly/domain/block/controller/BlockController.java
+++ src/main/java/com/sparta/filmfly/domain/block/controller/BlockController.java
@@ -1,18 +1,20 @@
 package com.sparta.filmfly.domain.block.controller;
 
 import com.sparta.filmfly.domain.block.dto.BlockRequestDto;
+import com.sparta.filmfly.domain.block.dto.BlockedUserResponseDto;
 import com.sparta.filmfly.domain.block.service.BlockService;
+import com.sparta.filmfly.domain.user.dto.UserResponseDto;
 import com.sparta.filmfly.global.auth.UserDetailsImpl;
+import com.sparta.filmfly.global.common.response.DataResponseDto;
 import com.sparta.filmfly.global.common.response.MessageResponseDto;
 import com.sparta.filmfly.global.common.response.ResponseUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users/block")  // 수정된 부분
@@ -29,5 +31,14 @@ public class BlockController {
     ) {
         blockService.blockUser(userDetails.getUser().getId(), blockRequestDto.getBlockedId(), blockRequestDto.getMemo());
         return ResponseUtils.success();
+    }
+
+    // 차단된 유저 목록 조회
+    @GetMapping
+    public ResponseEntity<DataResponseDto<List<BlockedUserResponseDto>>> getBlockedUsers(
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        List<BlockedUserResponseDto> blockedUsers = blockService.getBlockedUsers(userDetails.getUser());
+        return ResponseUtils.success(blockedUsers);
     }
 }
