6322459c084688c230c234ff9be05394e47347ba
원지연 <dnjs229@gmail.com>
2024-07-24 15:58:15 +0900

    [REFACTOR] 코드 컨벤션 적용 완료

--- src/main/java/com/sparta/filmfly/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/filmfly/domain/review/service/ReviewService.java
@@ -25,8 +25,11 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final MovieRepository movieRepository;
 
+    /**
+     * 리뷰 저장
+     */
     @Transactional
-    public ReviewResponseDto saveReview(User loginUser, ReviewCreateRequestDto requestDto) {
+    public ReviewResponseDto createReview(User loginUser, ReviewCreateRequestDto requestDto) {
         Movie findMovie = movieRepository.findById(requestDto.getMovieId())
             .orElseThrow(() -> new IllegalArgumentException("영화 못 찾음")); // Movie 기능 올리면
 
@@ -36,15 +39,21 @@ public class ReviewService {
         return ReviewResponseDto.fromEntity(loginUser, savedReview);
     }
 
+    /**
+     * 리뷰 단일 조회
+     */
     @Transactional(readOnly = true)
-    public ReviewResponseDto findReview(Long reviewId) {
+    public ReviewResponseDto getReview(Long reviewId) {
         Review findReview = reviewRepository.findByIdOrElseThrow(reviewId);
 
         return ReviewResponseDto.fromEntity(findReview.getUser(), findReview);
     }
 
+    /**
+     * 특정 영화에 대한 리뷰 전체 조회
+     */
     @Transactional(readOnly = true)
-    public List<ReviewResponseDto> findReviews(Long movieId, Pageable pageable) {
+    public List<ReviewResponseDto> getPageReview(Long movieId, Pageable pageable) {
         Page<Review> findReviews = reviewRepository.findByMovieId(movieId, pageable);
         List<ReviewResponseDto> responseDtos = new ArrayList<>();
         for (Review review : findReviews) {
@@ -55,20 +64,28 @@ public class ReviewService {
         return responseDtos;
     }
 
+    /**
+     * 리뷰 수정
+     */
     @Transactional
     public ReviewResponseDto updateReview(User loginUser, ReviewUpdateRequestDto requestDto, Long reviewId) {
         Review findReview = reviewRepository.findByIdOrElseThrow(reviewId);
 
+        // 수정하려는 리뷰가 자기가 작성한 리뷰인지 확인
         findReview.checkReviewOwner(loginUser);
 
         findReview.updateReview(requestDto);
         return ReviewResponseDto.fromEntity(findReview.getUser(), findReview);
     }
 
+    /**
+     * 리뷰 삭제
+     */
     @Transactional
     public void deleteReview(User loginUser, Long reviewId) {
         Review findReview = reviewRepository.findByIdOrElseThrow(reviewId);
 
+        // 삭제하려는 리뷰가 자기가 작성한 리뷰인지 확인
         findReview.checkReviewOwner(loginUser);
 
         reviewRepository.delete(findReview);
