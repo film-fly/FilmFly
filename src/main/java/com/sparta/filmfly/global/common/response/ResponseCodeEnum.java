f2e56461b82e4bb0396cec9a62d0597b8da19aab
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-26 15:36:28 +0900

    [REFACTOR] 이넘 메세지 주석처리 수정

--- src/main/java/com/sparta/filmfly/global/common/response/ResponseCodeEnum.java
+++ src/main/java/com/sparta/filmfly/global/common/response/ResponseCodeEnum.java
@@ -1,5 +1,6 @@
 package com.sparta.filmfly.global.common.response;
 
+import com.sparta.filmfly.domain.report.entity.Report;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -7,21 +8,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ResponseCodeEnum {
-    /**
-     * Media
-     */
+
+    // Common
     UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
 
-    /**
-     * Auth
-     */
+    // Auth
     INVALID__REQUEST(HttpStatus.NOT_FOUND, "해당 요청을 처리 할 수 없습니다."),
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인 실패"),
     INVALID_TOKENS(HttpStatus.BAD_REQUEST, "유효하지 않는 토큰입니다."),
 
-    /**
-     * User
-     */
+    // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     USER_ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "사용자는 이미 인증되었습니다."),
@@ -37,18 +33,12 @@ public enum ResponseCodeEnum {
     EMAIL_RESEND_LIMIT(HttpStatus.BAD_REQUEST, "한 시간내에 5회 까지만 요청이 가능합니다"),
     EMAIL_VERIFICATION_TOKEN_MISMATCH(HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다."),
 
-    /**
-     * Report
-     */
+    // Report
     ALREADY_REPORTED(HttpStatus.BAD_REQUEST, "이미 신고 처리 되었습니다."),
 
-    /**
-     * Block
-     */
+    //Block
     USER_ALREADY_BLOCKED(HttpStatus.BAD_REQUEST, "이미 차단된 사용자입니다."),
     NOT_BLOCKED_TARGET(HttpStatus.NOT_FOUND, "차단된 상대가 아닙니다."),
-
-
 
 
     ;
