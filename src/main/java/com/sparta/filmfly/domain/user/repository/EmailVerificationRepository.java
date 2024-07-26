c5a969e38e82b933a9a41672fe9ce5ae893e7ac9
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-26 15:28:18 +0900

    [REFACTOR] 이메일 인증 예외 메세지 수정

--- src/main/java/com/sparta/filmfly/domain/user/repository/EmailVerificationRepository.java
+++ src/main/java/com/sparta/filmfly/domain/user/repository/EmailVerificationRepository.java
@@ -21,6 +21,6 @@ public interface EmailVerificationRepository extends JpaRepository<EmailVerifica
 
     default EmailVerification findByUserOrElseThrow(User user) {
         return findByUser(user)
-                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.INVALID_VERIFICATION_TARGET));
+                .orElseThrow(() -> new NotFoundException(ResponseCodeEnum.USER_ALREADY_VERIFIED));
     }
 }
