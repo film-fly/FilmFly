0976e43799d385a93bd74bfb3cb3228b4608a47f
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-26 16:44:05 +0900

    Revert "[REFACTOR] 로그인 필터 예외처리 수정"
    This reverts commit dae6d9679017e418e01e430ece8085b10ce3f251.

--- src/main/java/com/sparta/filmfly/global/exception/custom/detail/NotFoundException.java
+++ src/main/java/com/sparta/filmfly/global/exception/custom/detail/NotFoundException.java
@@ -2,7 +2,6 @@ package com.sparta.filmfly.global.exception.custom.detail;
 
 import com.sparta.filmfly.global.common.response.ResponseCodeEnum;
 import com.sparta.filmfly.global.exception.custom.GlobalException;
-import lombok.Getter;
 
 public class NotFoundException extends GlobalException {
     public NotFoundException(ResponseCodeEnum responseCodeEnum) {
