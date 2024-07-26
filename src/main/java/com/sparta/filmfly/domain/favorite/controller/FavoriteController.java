df3e130e5ca1110aee455fd89e2cebbd9ee6709f
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-07-26 09:03:11 +0900

    [FEAT] 찜 삭제

--- src/main/java/com/sparta/filmfly/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/sparta/filmfly/domain/favorite/controller/FavoriteController.java
@@ -1,28 +1,16 @@
 package com.sparta.filmfly.domain.favorite.controller;
 
-import com.sparta.filmfly.domain.favorite.entity.Favorite;
 import com.sparta.filmfly.domain.favorite.service.FavoriteService;
 import com.sparta.filmfly.domain.movie.dto.MovieResponseDto;
-import com.sparta.filmfly.domain.review.dto.ReviewResponseDto;
-import com.sparta.filmfly.domain.review.entity.Review;
 import com.sparta.filmfly.global.auth.UserDetailsImpl;
 import com.sparta.filmfly.global.common.response.DataResponseDto;
 import com.sparta.filmfly.global.common.response.MessageResponseDto;
 import com.sparta.filmfly.global.common.response.ResponseUtils;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.ResponseUtil;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -57,5 +45,18 @@ public class FavoriteController {
         log.info("get favorite");
         List<MovieResponseDto> movieResponseDtoList = favoriteService.getFavorite(userDetails.getUser());
         return ResponseUtils.success(movieResponseDtoList);
+    }
+
+    /**
+    * 찜 취소하기
+    */
+    @DeleteMapping
+    public ResponseEntity<MessageResponseDto> deleteFavorite(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam Long movieId
+    ) {
+        log.info("delete favorite");
+        favoriteService.deleteFavorite(userDetails.getUser(), movieId);
+        return ResponseUtils.success();
     }
 }
