16c3c416c4d1f3983b31e531e326a8c2ec9ad8ac
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-08-02 12:44:12 +0900

    [BUGFIX] 활성화 상태 오타 수정

--- src/main/java/com/sparta/filmfly/domain/user/entity/UserStatusEnum.java
+++ src/main/java/com/sparta/filmfly/domain/user/entity/UserStatusEnum.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum UserStatusEnum {
-    VERIFIED("AVTIVE"), // 활성화 상태
+    VERIFIED("ACTIVE"), // 활성화 상태
     SUSPENDED("SUSPENDED"), // 정지된 상태
     DELETED("DELETED"); // 탈퇴한 상태
 
