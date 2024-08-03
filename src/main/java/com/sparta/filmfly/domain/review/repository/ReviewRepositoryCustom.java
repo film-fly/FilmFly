ede5c23c9f47744199bf0d6731a1d05914bbf70e
원지연 <dnjs229@gmail.com>
2024-08-03 21:44:15 +0900

    [FEAT] 영화 단건 조회 시 리뷰의 평균 평점 구현

--- src/main/java/com/sparta/filmfly/domain/review/repository/ReviewRepositoryCustom.java
+++ src/main/java/com/sparta/filmfly/domain/review/repository/ReviewRepositoryCustom.java
@@ -7,4 +7,5 @@ import org.springframework.data.domain.Pageable;
 
 public interface ReviewRepositoryCustom {
     PageResponseDto<List<ReviewResponseDto>> getPageReviewByMovieId(Long movieId, Pageable pageable);
+    Float getAverageRatingByMovieId(Long movieId);
 }
