00f0d68990425491b46be197792c082ef1d5f271
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-08-05 15:23:02 +0900

    Update TestController.java

--- src/main/java/com/sparta/filmfly/domain/test/TestController.java
+++ src/main/java/com/sparta/filmfly/domain/test/TestController.java
@@ -10,23 +10,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
-@RequestMapping("/test")
 @Controller
 public class TestController {
 
-    @GetMapping("/1")
+    @GetMapping("/")
+    public ResponseEntity<String> test0() {
+        log.info("test0");
+        return ResponseEntity.ok("00000000000000000000000000");
+    }
+
+    @GetMapping("/test/1")
     public String test1() {
         log.info("test1");
         return "test2";
     }
 
-    @GetMapping("/2")
+    @GetMapping("/test/2")
     public ResponseEntity<String> test2() {
         log.info("test2");
         return ResponseEntity.ok("이것은 테스트2의 성공인 것이야");
     }
 
-    @GetMapping("/3")
+    @GetMapping("/test/3")
     public ResponseEntity<DataResponseDto<String>> test3() {
         log.info("test3");
         return ResponseUtils.success("테스트3의 성공");
