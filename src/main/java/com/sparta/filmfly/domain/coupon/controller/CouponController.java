28e7878c283840e3cf9c568652a69add2c953a95
cthj1234 <ghwls8172@naver.com>
2024-08-06 00:35:15 +0900

    [REFACTOR]: 유저ID가 가지고있는 모든 쿠폰 조회 할 수 있도록 기능 구현

--- src/main/java/com/sparta/filmfly/domain/coupon/controller/CouponController.java
+++ src/main/java/com/sparta/filmfly/domain/coupon/controller/CouponController.java
@@ -1,15 +1,20 @@
 package com.sparta.filmfly.domain.coupon.controller;
 
+import com.sparta.filmfly.domain.coupon.dto.CouponPageResponseDto;
 import com.sparta.filmfly.domain.coupon.dto.CouponRequestDto;
 import com.sparta.filmfly.domain.coupon.dto.CouponResponseDto;
 import com.sparta.filmfly.domain.coupon.service.CouponService;
+import com.sparta.filmfly.domain.officeboard.dto.OfficeBoardPageResponseDto;
 import com.sparta.filmfly.global.auth.UserDetailsImpl;
 import com.sparta.filmfly.global.common.response.DataResponseDto;
+import com.sparta.filmfly.global.common.response.PageResponseDto;
 import com.sparta.filmfly.global.common.response.ResponseUtils;
+import com.sparta.filmfly.global.util.PageUtils;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -74,12 +80,28 @@ public class CouponController {
     }
 
     /**
+     * 유저의 발급받은 쿠폰 목록
+     */
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<DataResponseDto<CouponPageResponseDto>> getUserCoupons(
+            @PathVariable Long userId,
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "createdAt") String sortBy,
+            @RequestParam(required = false, defaultValue = "false") boolean isAsc
+    ) {
+        Pageable pageable = PageUtils.of(page, size, sortBy, isAsc);
+        CouponPageResponseDto responseDto = couponService.getUserCoupon(userId, pageable);
+        return ResponseUtils.success(responseDto);
+    }
+
+    /**
      * (이벤트) 유저에게 쿠폰 발급
      */
     @PostMapping("/event")
     public ResponseEntity<DataResponseDto<CouponResponseDto>> distributeCoupons(
             @AuthenticationPrincipal UserDetailsImpl userDetails
-    ){
+    ) {
         CouponResponseDto responseDto = couponService.distributeCoupons(userDetails.getUser());
         return ResponseUtils.success(responseDto);
     }
