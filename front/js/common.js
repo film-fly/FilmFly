b3f92f5052a2066e5986f7766d99ee1d9ec63117
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-08-08 20:48:00 +0900

    [REFACTOR] 로그인 만료시간 설정

--- front/js/common.js
+++ front/js/common.js
@@ -6,6 +6,20 @@ $(function () {
     // 로그인 상태 확인 함수
     function checkLoginStatus() {
       const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+      const loginTime = localStorage.getItem('loginTime');
+      const currentTime = new Date().getTime();
+
+      // 로그인 시간이 설정되어 있고, 현재 시간이 로그인 시간보다 크면 세션 만료
+      if (loginTime && currentTime > parseInt(loginTime)) {
+        if (localStorage.getItem('isLoggedIn') === 'true') {
+          localStorage.setItem('isLoggedIn', 'false');
+          localStorage.removeItem('userRole');
+          alert('세션이 만료되었습니다. 다시 로그인해 주세요.');
+          location.href = '../html/login.html';
+        }
+        return false;
+      }
+
       console.log('Check login status:', isLoggedIn);
       return isLoggedIn;
     }
@@ -43,6 +57,7 @@ $(function () {
               alert('로그아웃되었습니다.');
               localStorage.setItem('isLoggedIn', 'false'); // 로그인 상태 업데이트
               localStorage.removeItem('userRole'); // 사용자 역할 정보 제거
+              localStorage.removeItem('loginTime'); // 로그인 시간 제거
               updateAuthLinks();
               location.href = '../html/index.html';
             },
