50994385d5fb9606645ef10ac355d17dbd6db5b5
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-08-09 22:31:10 +0900

    [FEAT] 이메일 인증 캐시 사용 구현

---
+++ src/main/java/com/sparta/filmfly/global/config/MemcachedConfig.java
@@ -0,0 +1,35 @@
+package com.sparta.filmfly.global.config;
+
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.MemcachedClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class MemcachedConfig {
+
+    @Value("${memcached.host}")
+    private String memcachedHost;
+
+    @Value("${memcached.port}")
+    private int memcachedPort;
+
+    @Value("${memcached.timeout}")
+    private long memcachedTimeout;
+
+    @Bean
+    public MemcachedClient memcachedClient() throws IOException {
+        ConnectionFactoryBuilder builder = new ConnectionFactoryBuilder()
+                .setOpTimeout(memcachedTimeout); // 타임아웃 설정
+
+        List<InetSocketAddress> addresses = Collections.singletonList(new InetSocketAddress(memcachedHost, memcachedPort));
+
+        return new MemcachedClient(builder.build(), addresses);
+    }
+}
